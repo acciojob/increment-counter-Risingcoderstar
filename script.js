6f5bc3fa91d3e5f6c43a40dbf6d666a08d458ed8
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-03-18 16:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+let count=0;
+let btn=document.getElementById("incrementBtn");
+btn.addEventListener("click",()=>{
+alert("count is"+count);
+count+=1;
+let re=document.getElementById("counter");
+re.innerHTML=count;
+});
